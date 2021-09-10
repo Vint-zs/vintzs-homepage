332cc90440a430b77b8152a689a80530b7c35281
bini0823 <bini0823@naver.com>
2021-09-10 19:07:58 +0900

    Change ui

--- vintzs/src/components/Main.js
+++ vintzs/src/components/Main.js
@@ -35,7 +35,7 @@ class Main extends React.Component {
           <span className="image main">
             <img src={pic01} alt="" />
           </span>
-          <pre>
+          <span>
             Welcome to PetPal Village!<br/>
             <br/>
             In our village, various neighbors around the world are living.<br/>
@@ -49,7 +49,7 @@ class Main extends React.Component {
             · Mailing time depends on types of pets. Adjust the mailing time by selecting the pet<br/>
             · Pets will match you the right neighbors based on your interest and used language<br/>
             · Adopt diverse pets of your choice and customize their features
-          </pre>
+          </span>
           <p>
             <a href="#work">android</a> / 
             <a href="#work">ios</a>.
@@ -92,70 +92,88 @@ class Main extends React.Component {
             Let me introduce our people!
           </p>
             
-          <span className="image main">
-            <img src={pic03} alt="" />
-          </span>
-          <ul>
-            서 영 운
-            <br />
-            약력
-            <br />
-            <li>(현)서울대학교 bio intelligence lab 인턴</li>
-            <li>(전)키즈노트 안드로이드 개발자</li>
-            <li>중앙대학교 공학교육혁신센터 창업공모전 은상</li>
-            <li>(현)중앙대학교 소프트웨어전공</li>
-            <br />
-            담당
-            <li>대표 / 서버 개발</li>
-          </ul>
-          <span className="image main">
-            <img src={pic04} alt="" />
-          </span>
-          <ul>
-            권 현 빈
-            <br />
-            약력
-            <br />
-            <li>(현)엘리스 AI 트랙 전산학, 웹 프론트 실습 코치</li>
-            <li>(전)중앙대학교 인지각 연구실 SW개발 인턴</li>
-            <li>(전)하얀마인드 영어 교육 앱 개발 인턴</li>
-            <li>삼성SDS pro 알고리즘 자격증</li>
-            <li>(현)중앙대학교 컴퓨터공학전공</li>
-            <br />
-            담당
-            <li>앱/웹 개발</li>
-          </ul>
+          <div className="introduce">
+            <span className="image main">
+              <img src={pic03} alt="" />
+            </span>
+            <ul className="letter">
+              서 영 운
+              <br />
+              <br />
+              약력
+              <br />
+              <li>(현)서울대학교 bio intelligence lab 인턴</li>
+              <li>(전)키즈노트 안드로이드 개발자</li>
+              <li>중앙대학교 공학교육혁신센터 창업공모전 은상</li>
+              <li>(현)중앙대학교 소프트웨어전공</li>
+              <br />
+              담당
+              <li>대표 / 서버 개발</li>
+            </ul>
+          </div>
+          <br />
+          <br />
+          <div className="introduce">
+            <span className="image main">
+              <img src={pic04} alt="" />
+            </span>
+            <ul className="letter">
+              권 현 빈
+              <br />
+              <br />
+              약력
+              <br />
+              <li>(현)엘리스 AI 트랙 전산학, 웹 프론트 실습 코치</li>
+              <li>(전)중앙대학교 인지각 연구실 SW개발 인턴</li>
+              <li>(전)하얀마인드 영어 교육 앱 개발 인턴</li>
+              <li>삼성SDS pro 알고리즘 자격증</li>
+              <li>(현)중앙대학교 컴퓨터공학전공</li>
+              <br />
+              담당
+              <li>앱 / 웹 개발</li>
+            </ul>
+          </div>
+          <br />
+          <br />
+          <div className="introduce">
           <span className="image main">
             <img src={pic05} alt="" />
           </span>
-          <span>
-            오 소 윤
-            <br />
-            약력
-            <li>모은행 글로벌플랫폼부 재직</li>
-            <li>aT 폴란드사무소 인턴</li>
-            <li>두유요거트 오소야 창업</li>
-            <li>고려사이버대 AI 전공</li>
-            <li>한국외대 프랑스어 전공</li>
-            <br />
-            담당
-            <br />
-            <li>사업기획 / 마케팅</li>
-          </span>
-          <span className="image main">
-            <img src={pic06} alt="" />
-          </span>
-          <span>
-            김 준 오
-            <br />
-            약력
-            <li>(현) 인공지능 스타트업 근무</li>
-            <li>(전) sundosoft  개발팀 근무</li>
-            <li>(현) 중앙대 소프트웨어 전공</li>
-            <br />
-            담당
-            <li>ios 개발</li>
-          </span>
+            <ul ul className="letter">
+              오 소 윤
+              <br />
+              <br />
+              약력
+              <li>모은행 글로벌플랫폼부 재직</li>
+              <li>aT 폴란드사무소 인턴</li>
+              <li>두유요거트 오소야 창업</li>
+              <li>고려사이버대 AI 전공</li>
+              <li>한국외대 프랑스어 전공</li>
+              <br />
+              담당
+              <br />
+              <li>사업기획 / 마케팅</li>
+            </ul>
+          </div>
+          <br />
+          <br />
+          <div className="introduce">
+            <span className="image main">
+              <img src={pic06} alt="" />
+            </span>
+            <ul ul className="letter">
+              김 준 오
+              <br />
+              <br />
+              약력
+              <li>(현) 인공지능 스타트업 근무</li>
+              <li>(전) sundosoft  개발팀 근무</li>
+              <li>(현) 중앙대 소프트웨어 전공</li>
+              <br />
+              담당
+              <li>ios 개발</li>
+            </ul>
+          </div>
           {close}
         </article>
 
