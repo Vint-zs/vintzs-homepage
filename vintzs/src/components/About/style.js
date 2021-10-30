1941970e1e9fe2d7dd4cc87ff5127824b6de2959
chj878194 <chj878194@woowahan.com>
2021-10-30 17:54:30 +0900

    style: about page styling

--- vintzs/src/components/About/style.js
+++ vintzs/src/components/About/style.js
@@ -1,22 +1,28 @@
 import styled from 'styled-components'
 
 export const MemberInfor = styled.div`
-  .profile-img {    
-    margin-right: 20px;
+  justify-content: flex-start;
+  align-items: flex-start;
+  height: 360px;
+
+  .profile-img {
+    margin: 0px 20px 0px 0px;
   }
 
   .name {
-    margin: 0 0 0 0;
-    font-size: 1.25rem;
-  }
-
-  .title {
     margin: 0 0 0 0;
     font-weight: 600;
     font-size: 1.25rem;
   }
 
+  .title {
+    margin: 0 0 0 0;
+    font-size: 1.25rem;
+  }
+
   ul {
     list-style: none;
+    height: 100%;
+    margin-top: 16px;
   }
 `
