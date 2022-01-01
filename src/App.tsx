ae8ea7f633b7601ce0bbc393df32dbddfc133fd5
chj878194 <chj878194@naver.com>
2022-01-02 00:32:55 +0900

    feature(header): set out of page and useLocation

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,19 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Footer from 'components/Footer';
+import Header from 'components/Header';
 import AboutPage from 'pages/AboutPage';
 import LandingPage from 'pages/LandingPage';
 
 const App: React.FC = () => (
   <BrowserRouter>
+    <Header></Header>
     <Routes>
       <Route path="/" element={<LandingPage />} />
       <Route path="/about" element={<AboutPage />} />
+      <Route path="/contact" element={<AboutPage />} />
     </Routes>
+    <Footer />
   </BrowserRouter>
 );
 
--- src/pages/LandingPage/index.tsx
+++ src/pages/LandingPage/index.tsx
@@ -1,13 +1,6 @@
 import React from 'react';
 import SContainer from './style';
-import Footer from 'components/Footer';
-import Header from 'components/Header';
 
-const LandingPage = () => (
-  <SContainer>
-    <Header selected="home"></Header>
-    <Footer></Footer>
-  </SContainer>
-);
+const LandingPage = () => <SContainer></SContainer>;
 
 export default LandingPage;
