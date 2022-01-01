b1b2336946dfd1f1ef729494f38524a8331d0574
chj878194 <chj878194@naver.com>
2022-01-01 23:46:08 +0900

    feature(component): add header component

--- custom.d.ts
+++ custom.d.ts
@@ -1,3 +1,13 @@
+import 'styled-components';
+import type { Palette } from './src/GlobalStyle';
+
+declare module 'styled-components' {
+  export interface DefaultTheme {
+    palette: Palette;
+  }
+}
+
+/* eslint-disable @typescript-eslint/no-explicit-any */
 declare module '*.svg' {
   const content: any;
   export default content;
