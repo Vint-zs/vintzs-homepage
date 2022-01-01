71d0d5ee40ddab4b85b98c443f8ee572d86e5e83
chj878194 <chj878194@naver.com>
2022-01-01 19:22:43 +0900

    feature(route): add page example

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import MainPage from 'pages/MainPage';
+import AboutPage from 'pages/AboutPage';
+import LandingPage from 'pages/LandingPage';
 
 const App: React.FC = () => (
   <BrowserRouter>
     <Routes>
-      <Route path="/" element={<MainPage />} />
+      <Route path="/" element={<LandingPage />} />
+      <Route path="/about" element={<AboutPage />} />
     </Routes>
   </BrowserRouter>
 );
