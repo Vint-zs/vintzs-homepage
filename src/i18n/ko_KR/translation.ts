df78799322f6e75f2fd2216e2863a69ce8f64fbd
vinny.hyeon <vinny.hyeon@kakaoent.com>
2022-01-02 15:56:25 +0900

    텍스트 추가

--- src/i18n/ko_KR/translation.ts
+++ src/i18n/ko_KR/translation.ts
@@ -8,6 +8,15 @@ const koreanText = {
     introduce_title: '우리는 Vintzs 입니다.',
     introduce_first_string: '만드는 사람도, 쓰는 사람도 즐거운 서비스.',
     introduce_second_string: 'Vintzs는 새로운 서비스로 기쁨을 선물합니다.',
+    company_first_title: '마음껏 의견내기',
+    company_first_content:
+      '모든 구성원의 의견은 소중합니다.\n맘껏 상상하고 의견을 개진해요.\n소통으로 팀원, 서비스 모두의 성장을 장려합니다.',
+    company_second_title: '재미있기',
+    company_second_content:
+      '일은 즐거워야 한다고 굳게 믿고 있어요.\n그런 환경에서 우리의 퍼포먼스는 최고가 됩니다.',
+    company_third_title: '빠르게 해보기',
+    company_third_content:
+      '좋은 아이디어는 실행으로 완성됩니다.\n기획한 아이디어는 MVP로 빠르게 검증하는 것이\nVintzs가 가장 잘하는 일입니다.',
   },
   footer: {
     copyright: 'Copyright 2021. Vintzs all rights reserved.',
