d61fffb3c4d273004dc3fbbcd3185738eca93771
chj878194 <chj878194@naver.com>
2022-01-06 22:52:10 +0900

    feat(page): render footer always bottom of page and give min-height to page.

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,22 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { AppContainer } from './GlobalStyle';
 import Footer from 'components/Footer';
 import Header from 'components/Header';
 import AboutPage from 'pages/AboutPage';
+import ContactPage from 'pages/ContactPage';
 import LandingPage from 'pages/LandingPage';
 
 const App: React.FC = () => (
   <BrowserRouter>
     <Header />
-    <Routes>
-      <Route path="/" element={<LandingPage />} />
-      <Route path="/about" element={<AboutPage />} />
-      <Route path="/contact" element={<AboutPage />} />
-    </Routes>
+    <AppContainer>
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/about" element={<AboutPage />} />
+        <Route path="/contact" element={<ContactPage />} />
+      </Routes>
+    </AppContainer>
     <Footer />
   </BrowserRouter>
 );
